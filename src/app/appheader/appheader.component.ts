bd299ffb79bcb387f634792c2fdea4cd8725f470
FahimFerdous <fahimferdous119@gmail.com>
2019-05-15 14:07:30 +0600

    Sticky Navbar added

--- src/app/appheader/appheader.component.ts
+++ src/app/appheader/appheader.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare var $:any;
+
 @Component({
   selector: 'app-appheader',
   templateUrl: './appheader.component.html',
@@ -10,6 +13,22 @@ export class AppheaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  var yourNavigation = $(".header-container");
+  
+  var stickyDiv = "sticky";
+  var yourHeader = $('.header-top').height();
+
+$(window).scroll(function() {
+if( $(this).scrollTop() > yourHeader ) {
+  yourNavigation.addClass(stickyDiv);
+ 
+} else {
+  yourNavigation.removeClass(stickyDiv);
+  
+}
+
+});
   }
 
 }
